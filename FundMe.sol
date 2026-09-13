d6e622ef5e60faa29b1603a45e0d492969d38441
AliTalalEl-Abur <ali549954@gmail.com>
2026-09-13 16:56:03 +0200

    commit

--- FundMe.sol
+++ FundMe.sol
@@ -6,9 +6,12 @@
 
 pragma solidity ^0.8.1;
 
-contract FundMe
-{
-    function fund() public {}
-
-    function withfraw() public {}
+contract FundMe {
+    function fund() public payable {
+    // Allow users to send $
+    // Have a minimum $ sent
+    // 1. How do we send ETH to this contract?
+    require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 **18
+    }
+    //function withfraw() public {}
 }
