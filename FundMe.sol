7bf1dcedbfbe6b61754951ee5ed9a8b2ac245178
AliTalalEl-Abur <ali549954@gmail.com>
2026-09-13 14:42:01 +0200

    c

--- FundMe.sol
+++ FundMe.sol
@@ -1,1 +1,14 @@
-bbb
+// Get funds from users
+// Withdraw
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.1;
+
+contract FundMe
+{
+    function fund() public {}
+
+    function withfraw() public {}
+}
