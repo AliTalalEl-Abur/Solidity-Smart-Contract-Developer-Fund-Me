9e75ff5c8cc23684feaf1fcfbbdb955e5a0e4cdc
AliTalalEl-Abur <ali549954@gmail.com>
2026-09-13 17:32:26 +0200

    c

--- FundMe.sol
+++ FundMe.sol
@@ -7,11 +7,22 @@
 pragma solidity ^0.8.1;
 
 contract FundMe {
+
+    //uint256 public myValue = 1;
+    uint256 public minimumUsd = 5;
+
     function fund() public payable {
     // Allow users to send $
-    // Have a minimum $ sent
+    // Have a minimum $ sent $5
     // 1. How do we send ETH to this contract?
-    require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 **18
+    //myValue = myValue + 2;
+    require(msg.value > minimumUsd, "didn't send enough ETH");//1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 **18
+    
+    // What is a revert?
+    // Undo any actions tha have been done, and send the remaining gas back
+
     }
+
+
     //function withfraw() public {}
 }
