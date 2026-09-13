1ad369378f2daf2c6ec7227530d0213d43912917
AliTalalEl-Abur <ali549954@gmail.com>
2026-09-13 19:17:48 +0200

    g

--- FundMe.sol
+++ FundMe.sol
@@ -14,13 +14,17 @@ contract FundMe {
     //uint256 public myValue = 1;
     uint256 public minimumUsd = 5e18;
 
+    address[] public funders;
+    mapping(address funder=> uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable {
     // Allow users to send $
     // Have a minimum $ sent $5
     // 1. How do we send ETH to this contract?
     //myValue = myValue + 2;
     require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ETH");//1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 **18
-    
+    funders.push(msg.sender);
+    addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     // What is a revert?
     // Undo any actions tha have been done, and send the remaining gas back
 
